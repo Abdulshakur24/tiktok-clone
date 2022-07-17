7166b8150922f4a8b144917239138bc6b3624e79
abdulshakur <abdulshakur486@gmail.com>
2022-07-17 14:39:19 +0300

    improved UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,9 +31,9 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+    <>
       <Link href="/">
-        <div className="w-[40px] flex items-center">
+        <div className="w-[30px] md:w-[40px] flex items-center">
           <Image
             width={"100%"}
             height={"100%"}
@@ -48,17 +48,17 @@ const Navbar = () => {
       <div className="relative hidden md:block">
         <form
           onSubmit={handleSearch}
-          className="absolute md:static top-10 -left-20 bg-white"
+          className="h-[40px] flex items-center bg-white"
         >
           <input
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
-            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0"
+            className="h-full bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full"
             placeholder="Search accounts and videos"
           />
           <button
             type="submit"
-            className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
+            className="translate-x-[-60px] md:right-5 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
           >
             <BiSearch />
           </button>
@@ -69,27 +69,27 @@ const Navbar = () => {
           <div className="flex gap-5 md:gap-10 items-center">
             <Link href="/upload">
               <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
-                <IoMdAdd className="text-xl" />{" "}
+                <IoMdAdd className="text-xl" />
                 <span className="hidden md:block">Upload </span>
               </button>
             </Link>
             {user.image && (
               <Link href={`/profile/${user._id}`}>
-                <div>
+                <div className="w-[30px] md:w-[40px]">
                   <Image
                     priority
                     className="rounded-full cursor-pointer"
                     src={user.image}
                     alt="user"
-                    width={40}
-                    height={40}
+                    width={"100%"}
+                    height={"100%"}
                   />
                 </div>
               </Link>
             )}
             <button
               type="button"
-              className="w-[40px] h-[40px] flex items-center justify-center border-[1px] p-2  rounded-full cursor-pointer outline-none shadow-md"
+              className="w-[30px] h-[30px] md:w-[40px] md:h-[40px] flex items-center justify-center border-[1px] p-2  rounded-full cursor-pointer outline-none shadow-md"
               onClick={() => {
                 googleLogout();
                 removeUser();
@@ -105,7 +105,7 @@ const Navbar = () => {
           />
         )}
       </div>
-    </div>
+    </>
   );
 };
 
